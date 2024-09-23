8841d04860356e2d4a46dc8eec24cd95ae1eaa6d
ToniPi89 <tonipinjuh89@gmail.com>
2024-09-23 19:35:22 +0200

    add-backend-url4

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "https://1354f8da-69a1-4ede-8e23-3ca811b076b7-dev.e1-eu-north-azure.choreoapis.dev/notes/backend/v1";
+const apiUrl = "/choreo-apis/notes/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
